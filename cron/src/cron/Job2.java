6e88ad82a6d8b3b520a554ef7469ca347f5bab38
Sowmiya Nagarajan <sowmiyanagarajan98@gmail.com>
2022-10-10 16:49:24 +0200

    Update Job2.java

--- cron/src/cron/Job2.java
+++ cron/src/cron/Job2.java
@@ -11,6 +11,6 @@ public class Job2 implements Job{
 	
 	public void execute(JobExecutionContext context) throws JobExecutionException {
 		System.out.println("Job2 --->>> Time is " + new Date());
-		EmailUtil.sendEmail("sowmiyanagarajan98@gmail.com","Email Testing Subject2", "Email Testing Body2");
+		EmailUtil.sendEmail("<put-email-here>","Email Testing Subject2", "Email Testing Body2");
 		} 
 }
